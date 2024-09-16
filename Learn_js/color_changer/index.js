eb5c751cd977baa5c135ece63d5d4b15f5595583
romitpatel <patelromit6952@gmail.com>
2024-09-16 15:42:29 +0530

    first

---
+++ Learn_js/color_changer/index.js
@@ -0,0 +1,26 @@
+ function changeColor(id){
+    const color=id;
+    document.body.style.backgroundColor=id;
+ }
+
+//  const buttons = document.querySelectorAll('.button');
+//  const body = document.querySelector("body");
+//  buttons.forEach(function(button){
+//     button.addEventListener('click',function(e){
+//         console.log(e)
+//         console.log(e.target);
+        
+//         if(e.target.id === "grey"){
+//             body.style.backgroundColor = 'grey';
+//         }
+//         if(e.target.id === "white"){
+//             body.style.backgroundColor = 'white';
+//         }
+//         if(e.target.id === "blue"){
+//             body.style.backgroundColor = 'blue';
+//         }
+//         if(e.target.id === "yellow"){
+//             body.style.backgroundColor = 'yellow';
+//         }
+//     })
+//  });
