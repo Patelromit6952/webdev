404161c986db49c001a3c8feb76cb48dcb8f9f5a
romitpatel <patelromit6952@gmail.com>
2024-10-18 18:25:35 +0530

    js1

--- Learn_js/index.js
+++ Learn_js/index.js
@@ -13,8 +13,34 @@ const myarr2 = new Array(1,2,3,4)
 
 //slice  , splice
 
-console.log("A",myarr2)
- const myn1 = myarr2.slice(1,3)
- console.log("B",myn1)
- const myn2 = myarr2.splice(1,3)
- console.log("c",myn2)
+// console.log("A",myarr2)
+//  const myn1 = myarr2.slice(1,3)
+//  console.log("B",myn1)
+//  const myn2 = myarr2.splice(1,3)
+//  console.log("c",myn2)
+
+//  const marvel_heros = ["thor","ironman","spiderman"]
+//  const dc_heros = ["superman","flash","batman"]
+
+//  marvel_heros.push(dc_heros)  // add array into array
+// // const newarr = marvel_heros.concat(dc_heros) //concat return new array
+// const all_heros = [...marvel_heros,...dc_heros]  // spread operator
+
+// const newarr = [1,2,3,[4,5,6],7,[8,[9,10]]]
+// const real_array = newarr.flat(Infinity)
+// console.log(real_array)
+
+// console.log(Array.isArray("romit"))
+// console.log(Array.from("romit"))
+
+// console.log(Array.from({name:"romit"}))
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of(score1,score2,score3))
+
+
+
+
